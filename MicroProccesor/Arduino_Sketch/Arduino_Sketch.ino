48329c34ef277baf1763b6e9ceda2959615d601a
ahmedsa321A <ahmedsa321A@gmail.com>
2024-09-05 04:36:54 +0300

    Added LDR sensor

--- MicroProccesor/Arduino_Sketch/Arduino_Sketch.ino
+++ MicroProccesor/Arduino_Sketch/Arduino_Sketch.ino
@@ -7,22 +7,28 @@ static int motor_Driver_Enable = PIN_PB3;
 
 static int Servo_pin = PIN_PB2;
 
+int ldrpin=PIN_PC2;
+int testLED=PIN_PB4;
 Servo Door;
 
 void setup() {
   pinMode(motor_Driver_Input1, OUTPUT);
   pinMode(motor_Driver_Input2, OUTPUT);
   pinMode(motor_Driver_Enable, OUTPUT);
-
+  pinMode(testLED, OUTPUT);
+  pinMode(ldrpin,INPUT);
   Door.attach(Servo_pin);
 
-  OpenDoor();
-  delay(500);
-  OpenDoor();
+  //OpenDoor();
+  //delay(500);
+  //OpenDoor();
 
 
   digitalWrite(motor_Driver_Input1, HIGH);
   digitalWrite(motor_Driver_Input2, LOW);
+
+
+   digitalWrite(testLED,HIGH); 
 }
 
 void SetFanSpeed(int value) {
@@ -36,4 +42,11 @@ void OpenDoor() {
 }
 
 void loop() {
+
+  int ldrValue = analogRead(ldrpin);
+  if (ldrValue > 800) {
+    digitalWrite(testLED,LOW);
+  } else {
+    digitalWrite(testLED,HIGH);  
+  }
 }
